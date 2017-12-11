09ba8de32a93677d08317df00eed5d713e7b95b2
josecostamartins <josecostamartins@gmail.com>
2017-12-11 00:50:38 -0200

    copied template files from chibios
    updated readme.md
    updated bootloader_defs.h based on STM32L1XXXC specs

--- keyboards/anne_pro/bootloader_defs.h
+++ keyboards/anne_pro/bootloader_defs.h
@@ -2,6 +2,9 @@
 /* It is chip dependent, the correct number can be looked up here:
  * http://www.st.com/web/en/resource/technical/document/application_note/CD00167594.pdf
  * This also requires a patch to chibios:
- *  <tmk_dir>/tmk_core/tool/chibios/ch-bootloader-jump.patch
+ *  <tmk_dir>/tmk_core/tool/chibios/ch-bootloader-jump.patch  
+ * INVESTIGATE IF WE NEED THIS PATCH IS COMPATIBLE WITH STM32L1XXXC
+ * ALSO, CONSIDERING WE HAVE A DFU BOOTLOADER, THE PROBABLE DOCUMENTATION IS
+ * PERTINENT TO STM32L1XXXC IN THE AFOREMENTIONED DOCUMENT
  */
-#define STM32_BOOTLOADER_ADDRESS 0x1FFFD800
+#define STM32_BOOTLOADER_ADDRESS 0x1FF00000
